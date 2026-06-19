c1d7d6e1d1a989a46608352e0db78831e82d26af
Raj Kamal <rajkamaldhan@gmail.com>
2026-06-19 14:37:41 +0530

    feat: implement App routing structure with protected routes and authentication guards

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navigate, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/signIn";
+import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 // import serverUrl  from "./config";
 import CustomeAlert, { showCustomAlert } from "./components/CustomeAlert";
